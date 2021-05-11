02a3ee6e6c696f5456d99333496743e8341d045a
squach28 <squach3443@gmail.com>
2021-05-11 13:37:02 -0700

    removed file

---
+++ lib/views/SignupPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  @override 
+  SignupPageState createState() => SignupPageState();
+}
+
+class SignupPageState extends State<SignupPage> {
+
+  Widget _signUpForm() {
+    
+  }
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
