91c24fbcf80a6975e28cfef7f9f6fbff0792b546
squach28 <squach3443@gmail.com>
2021-06-03 23:39:16 -0700

    replaced add workout card with floating button

--- lib/views/WorkoutsPage.dart
+++ lib/views/WorkoutsPage.dart
@@ -1,4 +1,4 @@
-import 'package:fit_buddy/views/components/AddWorkoutCard.dart';
+import 'package:fit_buddy/views/AddWorkoutPage.dart';
 import 'package:flutter/material.dart';
 import 'package:fit_buddy/views/components/WorkoutCard.dart';
 
@@ -17,19 +17,24 @@ class WorkoutsPageState extends State<WorkoutsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Theme.of(context).primaryColor,
-        body: SafeArea(
-            child: Padding(
-                padding: EdgeInsets.only(top: 25.0),
-                child: Center(
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        workoutsPageHeader(),
-                        AddWorkoutCard(),
-                        WorkoutCard()
-                      ]),
-                ))));
+      backgroundColor: Theme.of(context).primaryColor,
+      body: SafeArea(
+          child: Padding(
+              padding: EdgeInsets.only(top: 25.0),
+              child: Center(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [workoutsPageHeader(), WorkoutCard()]),
+              ))),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          backgroundColor: Colors.green,
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => AddWorkoutPage()));
+          }),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
   }
 }
